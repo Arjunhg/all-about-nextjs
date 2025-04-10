04b02a3a9c5f42926f8ce9a643a177cbe5a2d6c9
Arjun Sharma <arjunsharma03.sa@gmail.com>
2025-04-11 00:35:50 +0530

    (feat): Nested routing example in Next.js

---
+++ src/app/02.NestedRoutes/01.Example/users/about/page.tsx
@@ -0,0 +1,10 @@
+
+const AboutUser = () => {
+  return (
+    <div>
+      About User Page
+    </div>
+  )
+}
+
+export default AboutUser
---
+++ src/app/02.NestedRoutes/01.Example/users/about/project/page.tsx
@@ -0,0 +1,10 @@
+// http://localhost:3000/02.NestedRoutes/01.Example/users/about/project
+const Project = () => {
+  return (
+    <div>
+      About User Project Page
+    </div>
+  )
+}
+
+export default Project
