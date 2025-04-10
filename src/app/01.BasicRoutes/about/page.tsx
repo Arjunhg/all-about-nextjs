fc4a5a40c458eeccc1cf7d37bc486ac2ad31bcaa
Arjun Sharma <arjunsharma03.sa@gmail.com>
2025-04-11 00:26:21 +0530

    (feat): Basics of routing in Next.js

---
+++ src/app/01.BasicRoutes/about/page.tsx
@@ -0,0 +1,11 @@
+
+// http://localhost:3000/01.BasicRoutes/about
+const About = () => {
+  return (
+    <div>
+      Content Inside About Page
+    </div>
+  )
+}
+
+export default About
---
+++ src/app/01.BasicRoutes/login/page.tsx
@@ -0,0 +1,11 @@
+// http://localhost:3000/01.BasicRoutes/login
+
+const Login = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Login
