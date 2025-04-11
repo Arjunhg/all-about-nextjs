8d738200af202eaa46d979ab1cddbc686ed00b73
Arjun Sharma <arjunsharma03.sa@gmail.com>
2025-04-12 04:01:35 +0530

    (feat): Special file, error, in Next.js

---
+++ src/app/13.Error/dashboard/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const ErrorBoundary = () => {
+    return(
+        <div>
+            Error in Dashboard
+        </div>
+    )
+}
+
+export default ErrorBoundary;
---
+++ src/app/13.Error/dashboard/page.tsx
@@ -0,0 +1,16 @@
+
+
+const Dashboard = () => {
+
+    if(2<5){
+        throw new Error('Error in Dashboard');
+    }
+
+    return(
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
